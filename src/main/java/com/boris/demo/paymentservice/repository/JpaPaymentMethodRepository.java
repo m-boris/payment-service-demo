86a5e705fdc871f9206bd65f36bea5e36f27f187
Boris Freidberg <boris.freidberg@gmail.com>
2020-02-06 22:30:35 +0200

    Remove import

--- src/main/java/com/boris/demo/paymentservice/repository/JpaPaymentMethodRepository.java
+++ src/main/java/com/boris/demo/paymentservice/repository/JpaPaymentMethodRepository.java
@@ -1,7 +1,6 @@
 package com.boris.demo.paymentservice.repository;
 
 import com.boris.demo.paymentservice.entity.PaymentMethodEntity;
-import com.boris.demo.paymentservice.model.PaymentMethod;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
