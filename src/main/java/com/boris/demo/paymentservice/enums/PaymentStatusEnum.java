62c4a675737074f45b644ad7c15c7a44133c39ba
Boris Freidberg <boris.freidberg@gmail.com>
2020-02-07 11:21:27 +0200

    Fix payment statuses add assertion

--- src/main/java/com/boris/demo/paymentservice/enums/PaymentStatusEnum.java
+++ src/main/java/com/boris/demo/paymentservice/enums/PaymentStatusEnum.java
@@ -1,5 +1,5 @@
 package com.boris.demo.paymentservice.enums;
 
 public enum PaymentStatusEnum {
-    PENDING, SUCCESS, FAIL
+    PENDING, ACCEPTED, REJECTED
 }
--- src/test/java/com/boris/demo/paymentservice/PaymentIntegrationTest.java
+++ src/test/java/com/boris/demo/paymentservice/PaymentIntegrationTest.java
@@ -1,19 +1,30 @@
 package com.boris.demo.paymentservice;
 
+import com.boris.demo.paymentservice.enums.PaymentStatusEnum;
+import com.boris.demo.paymentservice.model.Payment;
 import com.boris.demo.paymentservice.model.PaymentRequest;
 import com.boris.demo.paymentservice.model.PaymentMethod;
 import com.boris.demo.paymentservice.model.User;
+import lombok.SneakyThrows;
 import org.apache.commons.lang3.RandomUtils;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.util.Currency;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static com.boris.demo.paymentservice.enums.PaymentStatusEnum.REJECTED;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PaymentIntegrationTest implements IIntegrationTest {
 
 
+    public static final PaymentStatusEnum[] VALID_STATUSES = {PaymentStatusEnum.ACCEPTED, PaymentStatusEnum.REJECTED};
+
+    @SneakyThrows
     @Test
     public void paymentTest(){
         User payingUser = createUser();
@@ -28,7 +39,12 @@ public class PaymentIntegrationTest implements IIntegrationTest {
                 .currency(Currency.getInstance( "USD" ) )
                 .build();
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.postForObject(getFullUrl("/payment/init"), paymentRequest, UUID.class);
+        UUID paymentId = restTemplate.postForObject(getFullUrl("/payment/init"), paymentRequest, UUID.class);
+        // wait for risk engine
+        Thread.sleep( 1000 );
+        Payment payment = restTemplate.getForObject(getFullUrl("/payment/"+paymentId ), Payment.class);
+        PaymentStatusEnum paymentStatusEnum = payment.getPaymentStatus();
+         assertThat( paymentStatusEnum, Matchers.in( VALID_STATUSES ) );
     }
 
     private User createUser() {
